82cc55eeb5cbeebd4379d080fdb72062d20d564c
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-25 16:08:21 -0300

    Mostrando informações a mais sem infinito requisições

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+      loadedPost: null
+    }
+
+    componentDidUpdate() {
+      if (this.props.id) {
+        if (!this.state.loadedPost || this.state.loadedPost && this.state.loadedPost.id !== this.props.id) {
+          axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+              this.setState({loadedPost: response.data});
+            });
+        }
+      }
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Por favor, escolha um post!</p>;
         if(this.props.id) {
+           post = <p style={{textAlign: 'center'}}>Carregando...</p>;
+        }
+        if(this.state.loadedPost) {
           post = (
               <div className="FullPost">
-                  <h1>Título</h1>
-                  <p>Conteúdo</p>
+                  <h1>{this.state.loadedPost.Title}</h1>
+                  <p>{this.state.loadedPost.content}</p>
                   <div className="Edit">
                       <button className="Delete">Deletar</button>
                   </div>
