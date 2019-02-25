413d609a0119dcce780854fa069e1cae5add4529
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-25 17:30:58 -0300

    Enviando POST para o servidor

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -6,23 +7,35 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Lucas'
+    }
+
+    postDataHandler = () => {
+      const data = {
+        title: this.state.title,
+        body: this.state.content,
+        autor: this.state.author
+      }
+      axios.post('https://jsonplaceholder.typicode.com/posts/', data)
+        .then(response => {
+          console.log(response);
+        });
     }
 
     render () {
         return (
             <div className="NewPost">
                 <h1>Adicionar um Post</h1>
-                <label>Title</label>
+                <label>Título</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
+                <label>Conteúdo</label>
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
+                <label>Autor</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Lucas">Lucas</option>
+                    <option value="Fernando">Fernando</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Adicionar Post</button>
             </div>
         );
     }
