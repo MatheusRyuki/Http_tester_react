bbea836e5a70ca2bcddeaddbac5c8bc946255885
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-26 11:24:50 -0300

    Instâncias do Axios

---
+++ src/axios.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.headers.common['Authorization'] = 'AUTH TOKEN VERSION 2
+
+instance.interceptors.request.use(request => {
+  console.log(request);
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+instance.interceptors.response.use(response => {
+  console.log(response);
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+export default instance;
