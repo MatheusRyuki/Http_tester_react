a91e62106022e7a129b4aec72e4136a29aaae786
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-27 11:12:21 -0300

    Ajuste para router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 import './Blog.css';
 import Posts from '../Posts/Posts';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -15,7 +16,7 @@ class Blog extends Component {
                   </ul>
                 </nav>
               </header>
-              <Posts/>
+              <Route path="/" exact render={() => <Posts/>} />
             </div>
         );
     }
