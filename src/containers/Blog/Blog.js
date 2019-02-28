303384f9bc452cc8e9f2196021a88dc50d222dca
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-28 14:00:00 -0300

    Redirecionamento condicional

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import axios from '../../axios';
 import './Blog.css';
 import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Post from '../FullPost/FullPost'
 
 class Blog extends Component {
@@ -24,6 +24,7 @@ class Blog extends Component {
               <Switch>
                 <Route path="/new-post" component={NewPost}/>
                 <Route path="/posts/" component={Posts}/>
+                <Redirect from="/" to="/posts">
               </Switch>
             </div>
         );
