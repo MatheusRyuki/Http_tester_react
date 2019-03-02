39942b70910283ea23276d8eb76521a8d93978c3
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-02 11:43:41 -0300

    Carregamento Lazy de Componentes React

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
 import './Blog.css';
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
-import Post from '../FullPost/FullPost'
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('../NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-      auth: false
+      auth: true
     }
     render () {
         return (
@@ -25,7 +27,7 @@ class Blog extends Component {
                 </nav>
               </header>
               <Switch>
-                { this.state.auth ? <Route path="/new-post" component={NewPost}/> : null }
+                { this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null }
                 <Route path="/posts/" component={Posts}/>
                 <Redirect from="/" to="/posts" exact />
                 <Route render={() => <h1> Não achado </h1>} />
